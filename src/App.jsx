9529ce1a6902f3b8500718f261786436e443cb4f
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2023-10-29 22:41:28 +0800

    add a range input for length and checkbox for number

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,30 @@ function App() {
           <FaCopy />
         </button>
       </div>
+      <div className="flex flex-col text-m text-white font-bold gap-x-1">
+        <div className="flex justify-between items-center gap-x-1 py-2">
+          <label htmlFor="length">Length: {length}</label>
+          <input
+            type="range"
+            min={6}
+            max={24}
+            value={length}
+            className="cursor-pointer"
+            onChange={(e) => setLength(e.target.value)}
+            name="length"
+          />
+        </div>
+        <div className="flex justify-between items-center gap-x-1 py-2">
+          <label htmlFor="number">Numbers: </label>
+          <input
+            type="checkbox"
+            defaultChecked={allowNumber}
+            className="cursor-pointer"
+            onChange={() => setAllowNumber((prev) => !prev)}
+            name="number"
+          />
+        </div>
+      </div>
     </div>
   );
 }
