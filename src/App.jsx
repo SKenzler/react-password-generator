dc89bbd5c4c867b23e0c10d1d1d2ca3adfa99e7f
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2023-11-01 22:23:43 +0800

    add a generate password function to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import { useState } from "react";
+import { useState, useCallback, useEffect } from "react";
 import { FaCopy } from "react-icons/fa";
 import "./App.css";
 
 function App() {
   const [length, setLength] = useState(0);
-  const [allowNumber, setAllowNumber] = useState(false);
-  const [allowSymbols, setAllowSymbols] = useState(false);
+  const [allowNumbers, setAllowNumbers] = useState(false);
+  const [allowUppercase, setAllowUppercase] = useState(false);
   const [allowCharacters, setAllowCharacters] = useState(false);
   const [password, setPassowrd] = useState("");
+
+  const generatePassword = useCallback(() => {
+    let generatedPassword = "";
+    let allowableString = "abcdefghijklmnopqrstuvwxyz";
+
+    if (allowUppercase) allowableString += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (allowNumbers) allowableString += "0123456789";
+    if (setCharactersAllowed) allowableString += "!@#$%^&*()_+";
+
+    for (i = 1; i < length; i++) {
+      const index = Math.floor(Math.random() * generatedPassword.length + 1);
+      generatedPassword += allowableString.charAt(index);
+    }
+    setPassowrd(generatedPassword);
+  }, [length, allowUppercase, allowNumbers, allowCharacters]);
+
+  useEffect(() => {
+    generatePassword();
+  }, [length, allowUppercase, allowNumbers, allowCharacters]);
 
   return (
     <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-700 text-orange-500">
@@ -46,13 +65,33 @@ function App() {
           />
         </div>
         <div className="flex justify-between items-center gap-x-1 py-2">
+          <label htmlFor="uppercase">Uppercase: </label>
+          <input
+            type="checkbox"
+            defaultChecked={allowUppercase}
+            className="cursor-pointer"
+            onChange={() => setAllowUppercase((current) => !current)}
+            id="uppercaser"
+          />
+        </div>
+        <div className="flex justify-between items-center gap-x-1 py-2">
           <label htmlFor="number">Numbers: </label>
           <input
             type="checkbox"
-            defaultChecked={allowNumber}
+            defaultChecked={allowNumbers}
             className="cursor-pointer"
-            onChange={() => setAllowNumber((prev) => !prev)}
-            name="number"
+            onChange={() => setAllowNumbers((current) => !current)}
+            id="number"
+          />
+        </div>
+        <div className="flex justify-between items-center gap-x-1 py-2">
+          <label htmlFor="characters">Characters: </label>
+          <input
+            type="checkbox"
+            defaultChecked={allowCharacters}
+            className="cursor-pointer"
+            onChange={() => setAllowCharacters((current) => !current)}
+            id="characters"
           />
         </div>
       </div>
